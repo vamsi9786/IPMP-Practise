2d199fff5712ddf3bd3a20c9f557578fa54dbd5e
vamsi9786 <vamsi6319@gmail.com>
2026-01-13 23:56:28 +0530

    Add function to rotate array elements
    Implement array rotation function to rotate elements by k positions.

--- Week 1/rotate_array.c
+++ Week 1/rotate_array.c
@@ -1,1 +1,15 @@
-#include <stdio.h>
+void rotate(int* nums, int numsSize, int k) {
+    int temp[k+1],x=0;
+    k=k%numsSize;
+    int* t2=malloc(sizeof(int)*numsSize);
+    for(int i=numsSize-k;i<numsSize;i++){
+        temp[x++]=nums[i];
+    }
+    for(int i=0;i<numsSize;i++){
+        if(i<k) t2[i]=temp[i];
+        else t2[i]=nums[i-k];
+    }
+    for(int i=0;i<numsSize;i++){
+        nums[i]=t2[i];
+    }
+}
