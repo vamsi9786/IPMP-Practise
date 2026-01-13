c590c615ea87777fbd2b92a5db55aefb8ad47913
vamsi9786 <vamsi6319@gmail.com>
2026-01-13 23:38:35 +0530

    Implement sortedSquares function for squared array

--- Week 1/squares_of_a_sorted_array.c
+++ Week 1/squares_of_a_sorted_array.c
@@ -1,1 +1,15 @@
-#include <stdio.h>
+int cmp(const void* a,const void* b){
+    return *(int*)a - *(int*)b;
+}
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int* arr=malloc(numsSize*sizeof(int));
+    int left=0,right=numsSize-1;
+    while(left<=right){
+        arr[left]=nums[left]*nums[left];
+        if(left!=right) arr[right]=nums[right]*nums[right];
+        left++;right--;
+    }
+    *returnSize=numsSize;
+    qsort(arr,numsSize,sizeof(int),cmp);
+    return arr;
+}
