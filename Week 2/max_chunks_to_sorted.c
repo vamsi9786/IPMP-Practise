01174102e8a42236c3c86a063ebe0b7c77c6bd50
vamsi9786 <vamsi6319@gmail.com>
2026-01-15 01:51:49 +0530

    Update max_chunks_to_sorted.c

--- Week 2/max_chunks_to_sorted.c
+++ Week 2/max_chunks_to_sorted.c
@@ -1,1 +1,10 @@
-#include <stdio.h>
+int maxChunksToSorted(int* arr, int arrSize) {
+    int c=0,max=arr[0];
+    for(int i=0;i<arrSize;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(max==i) c++;
+    }
+    return c;
+}
