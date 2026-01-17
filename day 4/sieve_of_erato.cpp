f6dd153a7560c20f8ea3ada7f2e25f3652291d6b
vamsi9786 <vamsi6319@gmail.com>
2026-01-18 00:15:42 +0530

    Refactor sieve of Eratosthenes implementation
    Refactor sieve function to improve clarity and efficiency.

--- day 4/sieve_of_erato.cpp
+++ day 4/sieve_of_erato.cpp
@@ -1,3 +1,21 @@
+//sieve of eratosthenes
+  vector<int> sieve(int n) {
+    vector<bool> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for(int p = 2; p*p <= n; p++) {
+        if(is_prime[p]) {
+            for(int multiple = p*p; multiple <= n; multiple += p)
+                is_prime[multiple] = false;
+        }
+    }
+    vector<int> primes;
+    for(int i = 2; i <= n; i++)
+        if(is_prime[i]) primes.push_back(i);
+    return primes;
+}
+
+
+
 class Solution {
   public:
     vector<int> sieve(int n) {
