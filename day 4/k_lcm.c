ce858dd5a2d6a06b510c392c3e498d2e5255de74
vamsi9786 <vamsi6319@gmail.com>
2026-01-17 23:11:15 +0530

    Add k_lcm.c for calculating LCM with inputs

---
+++ day 4/k_lcm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+typedef long long ll;
+int main(){
+     int t;
+     scanf("%d",&t);
+     while(t--){
+          ll n,k;
+          scanf("%lld %lld",&n,&k);
+          
+          for(int i=0;i<k-3;i++){
+               printf("%d ",1);
+          }
+          ll m=n-(k-3);
+          if(m%2==1){
+               printf("1 %lld %lld\n",(m-1)/2,(m-1)/2);
+          }
+          else{
+               if(m%4==0){
+                    printf("%lld %lld %lld\n",m/2,m/4,m/4);
+               }
+               else{
+                    printf("2 %lld %lld\n",(m-2)/2,(m-2)/2);
+               }
+          }
+     }
+}
