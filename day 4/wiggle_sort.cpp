75f4dc1ffb92b9095bd4360151a7f13f91deee6b
vamsi9786 <vamsi6319@gmail.com>
2026-01-18 14:38:51 +0530

    Implement wiggle sort algorithm in C++

---
+++ day 4/wiggle_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void wiggleSort(vector<int> &nums) {
+        int l=nums.size();
+        vector<int> a1(l/2+1),a2(l/2);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<l;i++){
+            if(l%2==0){
+                if(i<(l/2)) a1[i]=nums[i];
+                else a2[i-(l/2)]=nums[i];
+            }
+            else{
+                if(i<(l/2)+1) a1[i]=nums[i];
+                else a2[i-(l/2)-1]=nums[i];
+            }
+        }
+        int x=0,y=0;
+        for(int i=0;i<l;i++){
+            if(i%2==0) nums[i]=a1[x++];
+            else nums[i]=a2[y++];
+        }
+    }
+};
