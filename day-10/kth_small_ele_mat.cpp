2e8b1cab76a444edc4920fb537ed2b37378ebc9c
vamsi9786 <vamsi6319@gmail.com>
2026-02-20 23:53:23 +0530

    Enhance kth smallest element algorithm with methods
    Added explanation and implementation of counting elements less than mid using upper_bound and staircase method.

--- day-10/kth_small_ele_mat.cpp
+++ day-10/kth_small_ele_mat.cpp
@@ -1,5 +1,31 @@
 similar algo to median by in median req=(n*m)/2 but here req=k-1;
 
+for counting no of ele less than mid i.e count 
+we use 
+      1. upper_bound i.e using binary search (used if each row is sorted ,each col may or may not)
+            
+      2. stair-case method (used if each row is sorted and each col is sorted)
+          stair-case algo:
+                (i). Start from bottom-left (or top-right) 
+                (ii). Move:                 (moves right value increases , moves up value decreases)
+                        Right if mat[i][j] ≤ mid     add count+=(i+1)             
+                        Up if mat[i][j] > mid  
+                  //staircase method
+                  int countLessEqual(vector<vector<int>>& mat, int n, int mid) {
+                    int count = 0;
+                    int i = n - 1, j = 0;
+                
+                    while (i >= 0 && j < n) {
+                        if (mat[i][j] <= mid) {
+                            count += (i + 1);
+                            j++;
+                        } else {
+                            i--;
+                        }
+                    }
+                    return count;
+                }
+
 class Solution {
   public:
     int kthSmallest(vector<vector<int>> &mat, int k) {
